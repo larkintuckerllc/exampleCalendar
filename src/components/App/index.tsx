1f532f4c2ec58fb20e730b37e04c47cfd368612d
John Tucker <john@larkintuckerllc.com>
2020-02-24 19:15:41 -0500

    Done

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Button, Icon, StyleProvider, Text } from 'native-base';
 import MyIcon from '../../config/MyIcon';
+import Fontello from '../../config/Fontello';
 import styles from './styles';
 import getTheme from '../../../native-base-theme/components';
 import material from '../../../native-base-theme/variables/material';
@@ -14,7 +15,40 @@ const App: FC = () => {
           <Text>Hello World</Text>
         </Button>
         <Icon>
-          <MyIcon size={60} color="red" name="emo-happy" />
+          <Fontello size={60} color="red" name="workshop" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="upload" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="trial" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="trash" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="trash-solid" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="tracking-numbers" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="tag" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="surgery" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="surgery-sheet" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="star" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="signature" />
+        </Icon>
+        <Icon>
+          <Fontello size={60} color="red" name="show" />
         </Icon>
       </SafeAreaView>
     </StyleProvider>
---
+++ src/config/Fontello.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from './fontello.json';
+
+const Fontello = createIconSetFromFontello(fontelloConfig, 'fontello');
+export default Fontello;
